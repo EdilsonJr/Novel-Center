a6afec041b558270ed0859d56c4ea9967f88f320
Edilson <edilsoncljr@gmail.com>
2022-05-28 23:49:00 -0300

    filtros

--- novel/views.py
+++ novel/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
 from .models import Novel
+from django.db.models import Q, Count, Case, When
 
 
 class NovelIndex(ListView):
@@ -9,13 +10,26 @@ class NovelIndex(ListView):
     template_name = 'novel/index.html'
     context_object_name = 'novels'
 
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.order_by('-id').filter(publicado=True)
+        qs = qs.annotate(
+            qtd_comentarios=Count(
+                Case(
+                    When(comentario__publicado_comentario=True, then=1)
+                )
+            )
+        )
+
+        return qs
+
 
 class NovelBusca(NovelIndex):
-    pass
+    template_name = 'novel/novel_busca.html'
 
 
 class NovelCategoria(NovelIndex):
-    pass
+    template_name = 'novel/novel_categoria.html'
 
 
 class NovelDetalhes(UpdateView):
